dae35ad3ffa7ca4de1dcad16a6cd670fd00d4e88
Poom <77903156+poomMizan@users.noreply.github.com>
2021-12-14 16:00:19 +0700

    My Comment to Add File

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        //
+        $field = $request->validate([
+            'name' => ['required','string'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed'],
+        ]);
+        $user = User::create([
+            'name' => $field['name'],
+            'email' => $field['email'],
+            'password' => bcrypt($field['password']),
+        ]);
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return Response($response, 201);
+    }
+    public function login(Request $request)
+    {
+        //
+        $field = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+        // check email & password
+        $user = User::where('email', $field['email'])->first(); 
+        if ($user == false || Hash::check($field['password'], $user->password) == false ) {
+            return response(['message' => 'Bad creds'], 401);
+        }
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+        return response($response, 201);
+    }
+    public function logout()
+    {
+        //
+        auth()->user()->tokens()->delete();
+        return response(['message' => 'Token destroyed and logged out'], 200);
+    }
+}
